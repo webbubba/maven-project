034f974089c36a4993c02bbc64650baaf8659e33
webbubba <swhaffiliate@gmail.com>
2017-11-25 12:02:02 -0500

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,8 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ 
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
